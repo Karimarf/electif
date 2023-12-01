050e89c4df5d117d79a20961fe84119ef1e0786f
Karimarf <karimaarfaouim@gmail.com>
2023-12-01 10:52:13 +0100

    Update README and age.asm

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
-![IE](https://img.shields.io/badge/Internet%20Explorer-0076D6?style=for-the-badge&logo=Internet%20Explorer&logoColor=white)
 
-    Ce programme affiche bonjour lors de l'execution
-    
-    modifie le pour qu'il demande une chaîne de caractere et affiche le par la suite 
+![ASSEMBLY](https://img.shields.io/badge/Assembly-2F3134?style=for-the-badge&logo=ASSEMBLY&logoColor=white)
+#Description
+Le code permet de demander l'age de l'utilisateur
+
+#Link
+##Clone
+`git clone https://github.com/Karimarf/electif.git`
+
+##Documentation :
 https://www.developpez.net/forums/d577860/autres-langages/assembleur/printf-scanf-assembleur/
+
+##Compilateur Linux :
+https://cdn.discordapp.com/attachments/1155775427848175707/1156139608304783370/compile64?ex=65659f6a&is=65532a6a&hm=240d2d653e52b38bd964f584b2359891d6e14bf71aa533f410712d3122c3c2ce&
+
+##Compilateur windows :
+https://www.nasm.us/pub/nasm/releasebuilds/?C=M;O=D
+
--- src/print.asm
+++ src/print.asm
@@ -1,17 +1,51 @@
 extern printf
+extern scanf
 
 section .data
-    hello db 'Bonjour', 0
+    question: db "Quel âge as-tu ?",10,0
+    demande_age: db "Votre âge -> ", 0
+    format: db "%d", 0
+    si_inf: db "Tu es trop jeune.", 10, 0
+    si_sup: db "Bienvenue !", 10, 0
+
+section .bss    
+    age: resd 1 ; Assuming 4-byte integer
 
 section .text
-    global _start
+    global main
 
-    mov rdi , hello
-    mov rax,0
+main:
+    push rbp
+
+    mov rdi, question
+    mov rax, 0
     call printf
 
-    mov rax,0
-    mov rdi,60
+    mov rdi, demande_age
+    mov rax, 0
+    call printf
+
+    mov rdi, format
+    mov rsi, age
+    mov rax, 0
+    call scanf
+
+    mov eax, [age]
+    cmp eax, 18
+    jae supérieur
+
+    mov rdi, si_inf
+    mov rax, 0
+    call printf
+    jmp end
+
+supérieur:
+    mov rdi, si_sup
+    mov rax, 0
+    call printf
+
+end:
+    pop rbp
+    mov rax, 60
+    xor rdi, rdi
     syscall
-    ret
-    
